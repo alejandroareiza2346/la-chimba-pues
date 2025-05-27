2dc0636e0705cd10695ea4e072ef74772e01b614
Alejo-DB <alejandroareiza.workspace@gmail.com>
2025-05-27 16:36:38 -0500

    Initial commit

--- django_arms/settings.py
+++ django_arms/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,12 +78,15 @@ WSGI_APPLICATION = 'django_arms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+import dj_database_url
+
+DATABASE_URL = config('DATABASE_URL', default='')
+if not isinstance(DATABASE_URL, str):
+    DATABASE_URL = ''
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)
 }
+
 
 
 # Password validation
